6e3ee310ad39927d29bcaf4999eeeb077bd97db8
Czarek Golecki <cezary.golecki@o2.pl>
2024-08-22 09:58:39 +0200

    fix installation

--- setup_env.sh
+++ setup_env.sh
@@ -4,7 +4,7 @@ poetry install
 
 git clone https://github.com/czarobxm/fast-transformers.git
 cd fast-transformers
-poetry run python3 setup.py install
+srun --partition=common --qos=1gpu4h --gres=gpu:1 poetry run python3 setup.py install
 cd ..
 rm -r fast-transformers
 
