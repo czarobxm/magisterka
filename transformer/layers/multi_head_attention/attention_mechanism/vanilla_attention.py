4fce43fe7ae64485406cbcbf9621528cfb4b973d
Czarek Golecki <cezary.golecki@o2.pl>
2024-09-12 00:30:06 +0200

    fix prevoius previous commit

--- transformer/layers/multi_head_attention/attention_mechanism/vanilla_attention.py
+++ transformer/layers/multi_head_attention/attention_mechanism/vanilla_attention.py
@@ -26,9 +26,7 @@ class VanillaAttention(nn.Module):
         Multi-head reshaping - Split heads from size [B, L, D] to size: [B, Nh, L, Dh]
         """
         batch_size = query.size(0)
-        query = (
-            query.view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2),
-        )
+        query = query.view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
         key = key.view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
         value = value.view(batch_size, -1, self.num_heads, self.head_dim).transpose(1, 2)
 
