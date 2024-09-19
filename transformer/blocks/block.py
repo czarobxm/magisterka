fix hourglass - set middle layer to be causal

transformer/blocks/block.py
@@ -202,7 +202,7 @@ class Block(nn.Module):
         self,
         x: torch.Tensor,
         key_value: torch.Tensor = None,
-        causal: bool = False,
+        causal: bool = True,
         inference: bool = False,
     ) -> torch.Tensor:
         """Produces the output of the encoder block."""

transformer/blocks/hourglass.py
@@ -144,7 +144,9 @@ class HourglassBlock(nn.Module):
             x = downsample(x)
 
         # Middle block
-        x = self.decoder_chunks[n_downsampling_layers](x)
+        x = self.decoder_chunks[n_downsampling_layers](
+            x, causal=causal, inference=inference
+        )
 
         # Upsampling path
         for dec, upsample, residual in zip(

transformer/blocks/utils.py
@@ -35,7 +35,9 @@ class DownsamplingLayer(nn.Module):
         """
         batch_size, seq_len, _ = x.size()
         new_seq_len = seq_len // self.downsampling_factor
-        x = x.view(batch_size, new_seq_len, self.downsampling_factor * self.d_model)
+        x = x.contiguous().view(
+            batch_size, new_seq_len, self.downsampling_factor * self.d_model
+        )
         return self.linear(x)
 
 
@@ -70,7 +72,9 @@ class UpsamplingLayer(nn.Module):
         """
         batch_size, seq_len, _ = x.size()
         x = self.linear(x)
-        return x.view(batch_size, seq_len * self.upsampling_factor, self.d_model)
+        return x.contiguous().view(
+            batch_size, seq_len * self.upsampling_factor, self.d_model
+        )
 
 
 class ShiftRight(nn.Module):