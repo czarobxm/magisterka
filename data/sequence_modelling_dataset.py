d1a09ec963bd26ef7b648aa692e1878ea75f2145
Czarek Golecki <cezary.golecki@o2.pl>
2024-09-30 15:02:07 +0200

    tokenize items while indexing them

--- data/sequence_modelling_dataset.py
+++ data/sequence_modelling_dataset.py
@@ -22,7 +22,6 @@ class TextGenerationDataset(torch.utils.data.Dataset):
         tokenizer: PreTrainedTokenizer = None,
         max_length: int = 512,
         cache_dir: str = None,
-        prepare_dataset: bool = True,
         device: str = "cpu",
     ):
         super().__init__()
@@ -40,82 +39,29 @@ class TextGenerationDataset(torch.utils.data.Dataset):
         self._tolist()
         logging.info("Dataset converted to list")
 
-        if prepare_dataset:
-            logging.info("Preparing dataset")
-            self.prepare_dataset()
-
     def to(self, device: str):
         """Move data to device"""
         self.data = self.data.to(device)
 
     def __len__(self):
-        return len(self.data)
+        return int(len(self.data) // self.max_length)
 
     def __getitem__(self, index):
-        return self.data[index]
-
-    def prepare_dataset(self) -> None:
-        """Prepare dataset to be used in model"""
-        if self.data is not None:
-            self.tokenize()
-            logging.info("Dataset tokenized")
-            self.slice_sentences()
-            logging.info("Dataset sliced")
-            self.pad_sentences()
-            logging.info("Dataset padded")
-            self.shuffle()
-            logging.info("Dataset shuffled")
-            self.to(self.device)
-            logging.info("Dataset moved to device")
+        start_idx = index * self.max_length
+        end_idx = (index + 1) * self.max_length - 1
+        token_dict = self.tokenizer(
+            self.data[start_idx:end_idx],
+            padding="max_length",
+            truncation="longest_first",
+            max_length=self.max_length,
+            return_tensors="pt",
+        )
+        return token_dict["input_ids"].squeeze(0)
 
     def _tolist(self):
         if isinstance(self.data, str):
             return self.data
         self.data = [row["text"] for row in self.data]
-        return self.data
-
-    def tokenize(self) -> torch.Tensor:
-        """Use tokenizer to convert text to tokens"""
-        if self.tokenizer is None:
-            raise ValueError("No tokenizer provided")
-
-        token_dict = self.tokenizer(
-            self.data,
-            padding="do_not_pad",
-            truncation="do_not_truncate",
-            return_tensors="np",
-        )
-
-        data = token_dict["input_ids"].tolist()
-        self.data = [torch.Tensor(sentence) for sentence in data]
-        return self.data
-
-    def slice_sentences(self) -> torch.Tensor:
-        """Slice sentences into blocks of max_length"""
-        sliced_sentences = []
-        block_length = self.max_length
-        last_chunk = None
-        for sentence in self.data:
-            if last_chunk is not None:
-                sentence = torch.cat([last_chunk, sentence])
-            n_chunks = len(sentence) // block_length
-            last_chunk_len = len(sentence) % block_length
-            if last_chunk_len == 0:
-                slices_sizes = [block_length] * n_chunks
-                last_chunk = None
-            else:
-                slices_sizes = [block_length] * n_chunks + [last_chunk_len]
-            sliced_sentences.extend(torch.split(sentence, slices_sizes))
-            last_chunk = sliced_sentences.pop(-1)
-        self.data = torch.vstack(sliced_sentences)
-
-        return self.data
-
-    def pad_sentences(self) -> torch.Tensor:
-        """Pad sentences to max_length"""
-        self.data = self.tokenizer.pad({"input_ids": self.data}, padding="longest")[
-            "input_ids"
-        ].to(torch.long)
         return self.data
 
     def shuffle(self):
