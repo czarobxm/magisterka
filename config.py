f05252179a87effb1513e6ec39101caedf3fc331
Czarek Golecki <cezary.golecki@o2.pl>
2024-10-01 17:05:47 +0200

    fix previous commit

--- config.py
+++ config.py
@@ -17,11 +17,6 @@ SPECIAL_TOKENS_DICT = {
 
 
 DEFAULT_ARGS = {
-    # GLOBAL PARAMETERS
-    "task": "classification",
-    "seed": 42,
-    "device": "cuda",
-    #
     # NEPTUNE PARAMETERS
     "project": NEPTUNE_PROJECT,
     "api_token": NEPTUNE_API_TOKEN,
@@ -30,26 +25,27 @@ DEFAULT_ARGS = {
     "tags": [],
     #
     # TRAINING PARAMETERS
-    "lr": 0.00008,
+    "init_lr": 0.00008,
     "epochs": 6,
-    "batch_size": 8,
+    "batch_size": 2,
     "criterion": "cross_entropy",
     # SCHEDULER PARAMETERS
     "scheduler": True,
-    "scheduler_lr_warmup_steps": 10000,
-    "scheduler_num_all_steps": 1000000,
+    "scheduler_lr_warmup_steps": int(1_000_000_000 / (4096 * 2)) * 0.01,
+    "scheduler_num_all_steps": int(1_000_000_000 / (4096 * 2)),
     "scheduler_final_lr_fraction": 0.1,
     #
     # DATASET PARAMETERS
+    "task": "sequence_modelling",
     "dataset": "enwik8",
     "tokenizer": "bert-base-uncased",
+    "max_length": 4096,
     #
     # MULTIHEAD ATTENTION PARAMETERS
     "mha_type": "vanilla",
     "d_model": 512,
     "num_heads": 8,
     "dropout": 0.1,
-    "max_length": 4096,
     "has_outproj": True,
     "act_fun": "relu",
     "apply_rotary_pos_enc": False,
@@ -57,6 +53,7 @@ DEFAULT_ARGS = {
     #
     # MODEL PARAMETERS
     "model": "decoder_only",
-    "structure": "512x6",
+    "structure": "6x4096",
     "pos_enc_type": "learnable",
+    "device": "cuda",
 }
