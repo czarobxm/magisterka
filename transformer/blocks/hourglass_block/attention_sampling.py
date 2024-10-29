83cbed9bd3a3b91a150acb7a3db41f9ff2964e07
Czarek Golecki <cezary.golecki@o2.pl>
2024-10-29 15:08:29 +0100

    add linear layer before attention sampling

--- transformer/blocks/hourglass_block/attention_sampling.py
+++ transformer/blocks/hourglass_block/attention_sampling.py
@@ -12,6 +12,10 @@ class AttentionDownsampling(nn.Module):
 
         self.act_fun = act_fun
 
+        self.w_q = nn.Linear(self.d_model, self.d_model)
+        self.w_k = nn.Linear(self.d_model, self.d_model)
+        self.w_v = nn.Linear(self.d_model, self.d_model)
+
         self.norm1 = nn.LayerNorm(self.d_model)
         self.norm2 = nn.LayerNorm(self.d_model)
         self.avg_pool = nn.AvgPool1d(
@@ -32,12 +36,20 @@ class AttentionDownsampling(nn.Module):
         return self.avg_pool(attn_output.transpose(-1, -2)).transpose(-2, -1)
 
     def forward(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor):
+        # Linear projections
+        query = self.w_q(query)
+        key = self.w_k(key)
+        value = self.w_v(value)
+
+        # Activation functions
         query = self.act_fun(query)
         key = self.act_fun(key)
 
+        # Attention
         attn_output = query + self.attention(
             self.norm1(query), self.norm1(key), self.norm1(value)
         )
+        # Feedforward
         ffn_output = attn_output + self.ffn(self.norm2(attn_output))
 
         return ffn_output
@@ -50,6 +62,10 @@ class AttentionUpsampling(nn.Module):
         self.d_model = d_model
 
         self.act_fun = act_fun
+
+        self.w_q = nn.Linear(self.d_model, self.d_model)
+        self.w_k = nn.Linear(self.d_model, self.d_model)
+        self.w_v = nn.Linear(self.d_model, self.d_model)
 
         self.norm1 = nn.LayerNorm(self.d_model)
         self.norm2 = nn.LayerNorm(self.d_model)
@@ -70,11 +86,19 @@ class AttentionUpsampling(nn.Module):
         return attn_output.view(batch_size, seq_len, d_model)
 
     def forward(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor):
+        # Linear projections
+        query = self.w_q(query)
+        key = self.w_k(key)
+        value = self.w_v(value)
+
+        # Activation functions
         query = self.act_fun(query)
         key = self.act_fun(key)
 
+        # Attention
         attn_output = query + self.attention(
             self.norm1(query), self.norm1(key), self.norm1(value)
         )
+        # Feedforward
         ffn_output = attn_output + self.ffn(self.norm2(attn_output))
         return ffn_output
